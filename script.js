bd5f0d4c29a361464e5ee6a3e870cafa2ff429bc
tejasvairal1234 <tejasvairal7066@gmail.com>
2026-06-03 12:13:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
 
+const todo = document.getElementById("newTodoInput");
+const btn = document.getElementById("addTodoBtn");
+const list = document.getElementById("todoList");
+btn.addEventListener("click", () =>
+{
+  if(todo.value)
+  {
+    let item = document.createElement("li");
+    item.textContent = todo.value;
+    list.appendChild(item);
+  }
+  todo.value =""
+});
